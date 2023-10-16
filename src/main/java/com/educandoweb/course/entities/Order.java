5fea08b564c6fd0c68fe31d0e385e36d2ba85fa7
Celso JR <celsojunior788@outlook.com>
2023-10-16 16:35:54 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -24,19 +25,26 @@ public class Order implements Serializable {
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, 
 			pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	
+	
 	private Instant moment;
+	
+	
+	private Integer orderStatus;
+	public Order() {
+	}
+
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-
-	public Order() {
-	}
-
-	public Order(Long id, Instant moment, User client) {
+	
+	
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -54,7 +62,17 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}	
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
 	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+	}
+		}
 
 	public User getClient() {
 		return client;
